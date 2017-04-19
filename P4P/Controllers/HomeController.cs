60f26d5ffafff00b90435c6f8298dae060608efd
Sven Mark Hofstra <hofs1600@student.nhl.nl>
2017-04-19 19:44:34 +0200

    First form + data storage in db

--- P4P/Controllers/HomeController.cs
+++ P4P/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using P4P.Models;
 
 namespace P4P.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
@@ -22,7 +24,30 @@ namespace P4P.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Get in Touch!";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(Contact contact)
+        {
+            using (var ctx = new P4PContext())
+            {
+                //als de code hier ergens een error oplevert voert hij catch uit.
+                try
+                {
+                    ctx.Contacten.Add(contact);
+                    ctx.SaveChanges();
+                    ViewBag.Succes = true;
+
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    ViewBag.Succes = false;
+                }
+            }
 
             return View();
         }
---
+++ P4P/Models/Contact.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+
+namespace P4P.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Message { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [RegularExpression("^\\(?([0-9]{3})\\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Het nummer is niet geldig")]
+        public string PhoneNumber { get; set; }
+    }
+}
