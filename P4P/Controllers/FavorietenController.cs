1ce755ae6768417a83be9e9b1a671e1215b446cd
Sven Mark Hofstra <hofs1600@student.nhl.nl>
2017-06-07 20:03:44 +0200

    Add Favorieten

---
+++ P4P/Controllers/FavorietenController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P4P.Controllers
+{
+    public class FavorietenController : Controller
+    {
+        // GET: Favorieten
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (P4PContext ctx = new P4PContext())
+            {
+                var favorietenlijst = ctx.Favorietenlijsts.SingleOrDefault(m => m.Id == id);
+
+                if (favorietenlijst == null)
+                    return HttpNotFound();
+
+                return View(favorietenlijst);
+            }
+        }
+        //TODO: New, Edit, Delete, Save(controller voor opslaan van voorgaande acties), Add_product, removeproduct, editproduct?
+    }
+}
