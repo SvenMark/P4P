822feae8c547e9c75b6b6795664274ce26192d40
Sven Mark Hofstra <hofs1600@student.nhl.nl>
2017-06-07 21:12:41 +0200

    Add profiel

---
+++ P4P/Controllers/ProfielController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace P4P.Controllers
+{
+    public class ProfielController : Controller
+    {
+        // GET: Profiel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Gegevens()
+        {
+            using (P4PContext ctx = new P4PContext())
+            {
+                var gebruiker = ctx.Gebruikers.Find(25);
+//                var gebruiker = ctx.Gebruikers.Find(Session["id"]);
+                return View(gebruiker);
+            }
+        }
+
+        public ActionResult Bedrijf()
+        {
+            return View();
+        }
+
+        public ActionResult Contactpersonen()
+        {
+            return View();
+        }
+    }
+}
