fa2a3a67649109617edfcd04e231609d6c71691e
Kevin Snijder <snij1504@student.nhl.nl>
2017-06-23 17:07:34 +0200

    buggy commit

---
+++ P4P/Migrations/201706231458378_kaasisbaas.cs
@@ -0,0 +1,26 @@
+namespace P4P.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class kaasisbaas : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Subcategories", "Hoofdcategorie_Id", "dbo.Hoofdcategories");
+            DropIndex("dbo.Subcategories", new[] { "Hoofdcategorie_Id" });
+            AlterColumn("dbo.Subcategories", "Hoofdcategorie_Id", c => c.Int());
+            CreateIndex("dbo.Subcategories", "Hoofdcategorie_Id");
+            AddForeignKey("dbo.Subcategories", "Hoofdcategorie_Id", "dbo.Hoofdcategories", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Subcategories", "Hoofdcategorie_Id", "dbo.Hoofdcategories");
+            DropIndex("dbo.Subcategories", new[] { "Hoofdcategorie_Id" });
+            AlterColumn("dbo.Subcategories", "Hoofdcategorie_Id", c => c.Int(nullable: false));
+            CreateIndex("dbo.Subcategories", "Hoofdcategorie_Id");
+            AddForeignKey("dbo.Subcategories", "Hoofdcategorie_Id", "dbo.Hoofdcategories", "Id", cascadeDelete: true);
+        }
+    }
+}
